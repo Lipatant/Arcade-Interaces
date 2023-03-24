a279f4f3d9ebfe53f19e51e0980738dd91021932
Lipatant <virgile.berrier@gmail.com>
2023-03-24 17:17:02 +0100

    refactor: adding function shared::secToDelta in Shared/Time.hpp

--- Interfaces/Shared/Time.hpp
+++ Interfaces/Shared/Time.hpp
@@ -10,4 +10,8 @@
 namespace shared
 {
     using Delta = double;
+
+    inline shared::Delta secToDelta(shared::Delta const sec) \
+        { return sec * 1000000; }
+    inline shared::Delta secToDelta(void) { return shared::secToDelta(1); }
 }
