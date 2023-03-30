968def1c800dccc0a8056e158181e5b79f136660
Lipatant <virgile.berrier@gmail.com>
2023-03-30 17:45:20 +0200

    refactor: adding player name to Meta
    docs: updating the How Do I?.pdf to include informations about creating a game

--- Interfaces/Shared/Meta.hpp
+++ Interfaces/Shared/Meta.hpp
@@ -18,6 +18,7 @@ namespace shared
     private:
         std::string _loadGame = "";
         std::string _loadGrap = "";
+        std::string _loadName = "";
     public:
         /// @brief Stores a game filepath inside the members
         /// @param game Filepath to the shared library
@@ -25,6 +26,9 @@ namespace shared
         /// @brief Stores a graphical filepath inside the members
         /// @param graph Filepath to the shared library
         void storeGraphical(std::string const &graph) { _loadGrap = graph; }
+        /// @brief Stores a player name inside the members
+        /// @param name Player name
+        void storeName(std::string const &name) { _loadName = name; }
         /// @brief Extracts the stored game filepath and ERASE IT
         /// @return The extracted filepath
         std::string const extractGame(void) \
@@ -33,5 +37,9 @@ namespace shared
         /// @return The extracted filepath
         std::string const extractGraphical(void) \
             { std::string tmp = _loadGrap; _loadGrap = ""; return tmp; }
+        /// @brief Extracts the stored player name and ERASE IT
+        /// @return The extracted name
+        std::string const extractName(void) \
+            { std::string tmp = _loadName; _loadName = ""; return tmp; }
     };
 }
